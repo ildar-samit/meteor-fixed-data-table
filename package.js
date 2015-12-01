5b80d75684015526f119bfec0cbb6e0b8fad6c64
Ildar Samit <ildar.samit@gmail.com>
2015-12-01 17:02:18 -0500

    Upgrade to 0.6.0

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'ildar:fixed-data-table',
-  version: '0.4.7',
+  version: '0.6.0',
   summary: 'FixedDataTable React component packaged for Meteor',
   git: 'https://github.com/ildarsamit/meteor-fixed-data-table',
   documentation: 'README.md',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.2');
+  api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.use('react@0.1.13');
-  api.use('cosmos:browserify@0.5.1', 'client');
+  api.use('react@0.14.1_1');
+  api.use('cosmos:browserify@0.9.2', 'client');
   api.addFiles([
     'fixed-data-table.browserify.js',
     'fixed-data-table.browserify.options.json',
@@ -20,6 +20,6 @@ Package.onUse(function(api) {
 });
 
 Npm.depends({
-  'fixed-data-table': '0.4.7',
+  'fixed-data-table': '0.6.0',
   'externalify': '0.1.0',
 });
